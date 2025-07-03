0758f9c90568b64e0cc46d669750fef1c60a0aac
DylanMinda <dylant0909@gmail.com>
2025-07-02 20:29:44 -0500

    Avance de register

--- Spotify.MVC/Program.cs
+++ Spotify.MVC/Program.cs
@@ -1,23 +1,38 @@
+using Spotify.MVC.Interface;
+using Spotify.MVC.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Agregar servicios
 builder.Services.AddControllersWithViews();
+
+// Configurar HttpClient para API
+builder.Services.AddHttpClient<IUsuarioService, UsuarioService>();
+
+// Configurar sesiones
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Registrar servicios
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseSession(); // Importante: antes de UseAuthorization
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -25,3 +40,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
