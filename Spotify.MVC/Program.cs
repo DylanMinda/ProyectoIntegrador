0088e864d58e2a0e53695c52accc0aec0d9f358a
DylanMinda <dylant0909@gmail.com>
2025-07-03 01:33:46 -0500

    Home, register y login casi listos.

--- Spotify.MVC/Program.cs
+++ Spotify.MVC/Program.cs
@@ -1,43 +1,61 @@
+using Spotify.APIConsumer;
+using Spotify.Modelos;
 using Spotify.MVC.Interface;
 using Spotify.MVC.Services;
+using static System.Net.WebRequestMethods;
+
+CRUD<Cancion>.EndPoint = "https://localhost:7028/api/Canciones";
+CRUD<Usuario>.EndPoint = "https://localhost:7028/api/Usuarios"; 
+CRUD<Plan>.EndPoint = "https://localhost:7028/api/Planes"; 
+//CRUD<Suscripcion>.EndPoint = "https://localhost:7028/api/Suscripciones"; 
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Agregar servicios
+//Registrar Servicios
+//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//builder.Services.AddScoped<IMetodoPagoService, MetodoPagoService>();
+//builder.Services.AddScoped<ISuscripcionService, SuscripcionService>();
+//builder.Services.AddScoped<IPlanService, PlanService>();
+builder.Services.AddScoped<IAutorizarService, AutorizarService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+//builder.Services.AddScoped<IPerfilService, PerfilService>();
+
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Configurar HttpClient para API
-builder.Services.AddHttpClient<IUsuarioService, UsuarioService>();
+builder.Services.AddAuthentication("Cookies") //cokies
+    .AddCookie("Cookies", options =>
+    {
+        options.LoginPath = "/Login/Index"; // Ruta de inicio de sesión
 
-// Configurar sesiones
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
 
-// Registrar servicios
-builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+    });
+builder.Services.AddHttpContextAccessor(); // Para acceder al contexto HTTP en los servicios //cokies
 
 var app = builder.Build();
 
-// Configurar pipeline
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseAuthentication(); // Habilitar autenticación antes de usar routing//cookies
+
 app.UseRouting();
-app.UseSession(); // Importante: antes de UseAuthorization
-app.UseAuthorization();
+
+app.UseAuthorization();//cokies
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
 
